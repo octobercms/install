fa2d622dc002e8beb7446044475a827801a76399
Sam Georges <sam@daftspunk.com>
2013-12-23 19:05:17 +1100

    Create simple container

---
+++ install.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width">
+        <title>October Installation</title>
+        <link href="install_files/style.css" rel="stylesheet">
+        <script src="install_files/script.js"></script>
+    </head>
+    <body>
+        <header class="container">
+            <div class="row">
+                <div class="col-md-7">
+                    <h1>October</h1>
+                </div>
+                <div class="col-md-5">
+
+                    <div class="steps row">
+                        <div class="col-sm-4"><p>Step 1</p></div>
+                        <div class="col-sm-4"><p>Step 2</p></div>
+                        <div class="col-sm-4"><p>Step 3</p></div>
+                    </div>
+
+                </div>
+            </div>
+
+        </header>
+
+        <section class="title">
+            <div class="container">
+                <h2>Installer</h2>
+            </div>
+        </section>
+
+        <section class="body container">
+        </section>
+
+
+        <footer class="container">
+            <ul class="list-inline pull-right">
+                <li><a target="_blank" href="http://octobercms.com/">October</a></li>
+                <li><a target="_blank" href="http://octobercms.com/docs">Documentation</a></li>
+                <li><a target="_blank" href="http://octobercms.com/about">About</a></li>
+            </ul>
+        </footer>
+    </body>
+</html>
