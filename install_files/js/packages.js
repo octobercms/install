34d52c60f57eb021fbbc6cc15d149549d4863c21
Sam Georges <sam@daftspunk.com>
2014-01-08 19:39:13 +1100

    Packages can now be added on the fly Plugin -> Package Adds Lato webfont

--- install_files/js/packages.js
+++ install_files/js/packages.js
@@ -7,25 +7,33 @@ Installer.Pages.packageInstall.init = function() {
     $('#suggestedPluginsContainer').hide()
 
     var template = Mustache.compile([
-        '<p class="plugin-image"><img src="{{image}}" alt=""></p>',
-        '<p class="plugin-name">{{name}}</p>',
-        '<p class="plugin-description">{{description}}</p>'
+        '<div class="package-details">',
+        '<div class="package-image"><img src="{{image}}" alt=""></div>',
+        '<div class="package-name ">{{name}}</div>',
+        '<div class="package-description text-overflow">{{description}}</div>',
+        '</div>'
     ].join(''))
 
-    $('#packageSearchInput').typeahead({
-        name: 'packages',
-        valueKey: 'code',
-        remote: window.location.pathname + '?handler=onSearchPackages&query=%QUERY',
-        template: template,
-        minLength: 3,
-        limit: 5
-    })
-    .on('typeahead:opened', function(){
-        $('#packageSearchInput .tt-dropdown-menu').css('width', $('#packageSearchInput').width() + 'px')
-    })
-    .on('typeahead:selected', function(){
-        // alert('selected')
-    })
+    $('#packageSearchInput')
+        .typeahead({
+            name: 'packages',
+            valueKey: 'code',
+            remote: window.location.pathname + '?handler=onSearchPackages&query=%QUERY',
+            template: template,
+            minLength: 3,
+            limit: 5
+        })
+        .on('typeahead:opened', function(){
+            $('#packageSearchInput .tt-dropdown-menu').css('width', $('#packageSearchInput').width() + 'px')
+        })
+        .on('typeahead:selected', function(object, datum){
+            console.log('a')
+            $('#packageInstallForm').submit()
+        })
+        .on('keypress', function(e) {
+            if (e.keyCode == 13) // ENTER key
+                $('#packageInstallForm').submit()
+        })
 
     Installer.Pages.packageInstall.renderIncluded()
 
@@ -58,7 +66,7 @@ Installer.Pages.packageInstall.renderIncluded = function() {
     }
     else {
         $.each(includedPlugins, function(index, plugin){
-            $('#pluginList').renderPartial('packages/plugin', plugin, { append:true })
+            $('#pluginList').renderPartial('packages/package', plugin, { append:true })
         })
         pluginListEmpty.hide()
     }
@@ -79,6 +87,61 @@ Installer.Pages.packageInstall.renderSuggested = function() {
     }
 }
 
+Installer.Pages.packageInstall.searchSubmit = function() {
+    var searchInput = $('#packageSearchInput'),
+        searchValue = searchInput.val()
+
+    Installer.Pages.packageInstall.includePackage(searchValue)
+    searchInput.val('')
+}
+
 Installer.Pages.packageInstall.includePackage = function(code) {
-    
+    var includedPlugins = Installer.Pages.packageInstall.includedPlugins,
+        pluginListEmpty = $('#pluginListEmpty'),
+        pluginExists = false
+
+    $.each(includedPlugins, function(index, plugin){
+        if (plugin.code == code)
+            pluginExists = true
+    })
+
+    if (pluginExists)
+        return
+
+    $.sendRequest('onPluginDetails', { code: code })
+        .done(function(plugin){
+            Installer.Pages.packageInstall.includedPlugins.push(plugin)
+            pluginListEmpty.hide()
+            $('#pluginList').renderPartial('packages/package', plugin, { append:true })
+            Installer.Pages.packageInstall.hilightIncludedPackages()
+        })
+        .fail(function(data){
+            alert(data.responseText)
+        })
+}
+
+Installer.Pages.packageInstall.removePackage = function(code) {
+    var includedPlugins = Installer.Pages.packageInstall.includedPlugins,
+        pluginListEmpty = $('#pluginListEmpty')
+
+    $('#pluginList [data-code="'+code+'"]').fadeOut(500, function(){
+        $('[data-code="'+code+'"]').removeClass('package-included')
+        $(this).remove()
+
+        Installer.Pages.packageInstall.includedPlugins = includedPlugins = $.grep(includedPlugins, function(plugin) {
+            return plugin.code != code;
+        })
+
+        if (includedPlugins.length == 0) {
+            pluginListEmpty.show()
+        }
+    })
+
+}
+
+Installer.Pages.packageInstall.hilightIncludedPackages = function() {
+    var includedPlugins = Installer.Pages.packageInstall.includedPlugins
+    $.each(includedPlugins, function(index, plugin){
+        $('[data-code="'+plugin.code+'"]').addClass('package-included')
+    })
 }
