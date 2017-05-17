7f6b07bd75cdaed22652b17a52d89bbe0f9e12e7
Maria Vilaró <maria.vilaro@gmail.com>
2017-05-17 11:50:43 +0200

    check mod_rewrite, json and fileinfo

--- install.php
+++ install.php
@@ -124,7 +124,10 @@
                     { code: 'mbstringLibrary', label: 'Mbstring PHP Extension is required' },
                     { code: 'sslLibrary', label: 'OpenSSL PHP Extension is required' },
                     { code: 'zipLibrary', label: 'ZipArchive PHP Library is required' },
-                    { code: 'gdLibrary', label: 'GD PHP Library is required' }
+                    { code: 'gdLibrary', label: 'GD PHP Library is required' },
+                    { code: 'mod_rewrite', label: 'PHP mod_rewrite module is required' },
+                    { code: 'jsonLibrary', label: 'JSON PHP Extension is required' },
+                    { code: 'fileinfoLibrary', label: 'fileinfo PHP Extension is required' }
                 ]
 
                 /*
--- install_files/php/Installer.php
+++ install_files/php/Installer.php
@@ -96,6 +96,15 @@ class Installer
             case 'zipLibrary':
                 $result = class_exists('ZipArchive');
                 break;
+            case 'mod_rewrite':
+                $result = array_key_exists('HTTP_MOD_REWRITE', $_SERVER);
+                break;
+            case 'jsonLibrary':
+                $result = function_exists('json_decode');
+                break;
+            case 'fileinfoLibrary':
+                $result = extension_loaded('fileinfo') && function_exists('finfo_open');
+                break;
         }
 
         $this->log('Requirement %s %s', $checkCode, ($result ? '+OK' : '=FAIL'));
