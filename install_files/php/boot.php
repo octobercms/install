94f3864425b474482227e131d3a433cd45925463
Yves Riedener <git@reecube.com>
2018-10-09 09:27:54 +0200

    polyfill for php 5.2.7 added

--- install_files/php/boot.php
+++ install_files/php/boot.php
@@ -2,6 +2,16 @@
 
 define('OCTOBER_MINIMUM_PHP_VERSION_ID', 70000);
 
+/**
+ * PHP_VERSION_ID is available as of PHP 5.2.7, if our 
+ * version is lower than that, then emulate it
+ */
+if (!defined('PHP_VERSION_ID')) {
+    $version = explode('.', PHP_VERSION);
+
+    define('PHP_VERSION_ID', ($version[0] * 10000 + $version[1] * 100 + $version[2]));
+}
+
 /*
  * Check PHP version
  */
